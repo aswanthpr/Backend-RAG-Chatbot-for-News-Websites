7e7002f2fa3e4e3d405d9c85224c45845822c310
aswanthpr <aswanthpr555@gmail.com>
2025-09-19 19:39:13 +0530

    third commit

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,12 @@ import chatRoute from "./routes/chat.routes"
 import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
-// import { runIngestion } from "./ingestion";
 
  const app: Application = express();
 
 redisClient();
 qdrantClient();
-// prismaClient()
-// runIngestion()
+
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(express.json());
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,2 @@
 export {redisClient,redis} from './redis.config';
-export {prismaClient} from './db.config';
 export {getQdrant,qdrantClient,initQdrant} from "./qdrant.config";
