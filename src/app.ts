6d5e738a0b097883e7f69303632dc8d7c483bfec
aswanthpr <aswanthpr555@gmail.com>
2025-09-20 12:59:56 +0530

    add health check

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Application } from "express";
+import express, { type Application , Request, Response} from "express";
 import { qdrantClient,redisClient} from "./configs"
 import { corsOptions } from "./middlewares";
 import chatRoute from "./routes/chat.routes"
@@ -6,6 +6,7 @@ import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
 import "./utils/crone.job.util";
+import { HttpResponse, HttpStatus } from "./constants";
 
  const app: Application = express();
 
@@ -17,7 +18,9 @@ app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.get("/health",(_req:Request,res: Response)=> {
+    res.status(HttpStatus?.OK).json(HttpResponse?.SUCCESS);
+})
 app.use("/chat", chatRoute); 
 
 export default app;      
